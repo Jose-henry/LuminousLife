e137e5ea6f161546bfd865f6ff435b54a7aeed04
Oguibe Fortune Oluomachukwu <tech.eagle.labs@gmail.com>
2024-06-27 03:22:05 +0100

    changes

--- src/components/shared/Footer/footer.tsx
+++ src/components/shared/Footer/footer.tsx
@@ -6,44 +6,59 @@ export default function FooterDiv() {
   return (
     <div className={styles.footer_container}>
       <div className={styles.topDiv}>
-        <Image src="/assets/footer-img.svg" alt="Sun" width={100} height={100}></Image>
+        <div className={styles.extra}>
+          <Image src="/assets/footer-img.svg" alt="Sun" width={100} height={100} className={styles.sun}></Image>
+          <Image src="/assets/apply-img.svg" alt="Light" width={100} height={100} className={styles.light}></Image>
+        </div>
         <div className={styles.wrapper}>
           <div className={styles.links}>
-            <p>Home</p>
-            <p>About</p>
-            <p>Team</p>
-            <p>Contact</p>
+            <Link href="/" className={styles.a}>Home</Link>
+            <Link href="/about" className={styles.a}>About</Link>
+            <Link href="/team" className={styles.a}>Team</Link>
+            <Link href="/contact" className={styles.a}>Contact</Link>
           </div>
           <div className={styles.socials}>
             <div className={styles.message}>
-              <Image src="/assets/Message.svg" alt="Message icon" height={100} width={100}></Image>
-              <p>SEND US A</p>
-              <h3>MESSAGE</h3>
+              <Link href="/message">
+                <Image src="/assets/Message.svg" alt="Message icon" height={50} width={50}></Image>
+              </Link>
+              <div>
+                <p>SEND US A</p>
+                <h3>MESSAGE</h3>
+              </div>
             </div>
             <div className={styles.instagram}>
-              <Image src="/assets/Instagram.svg" alt="Message icon" height={100} width={100}></Image>
-              <p>FOLLOW US ON</p>
-              <h3>INSTAGRAM</h3>
+              <Link href="https://www.instagram.com/luminous_life_foundation?igsh=MzBpaWVrODV4Nmxs">
+              <Image src="/assets/Instagram.svg" alt="Message icon" height={50} width={50}></Image>
+              </Link>
+              <div>
+                <p>FOLLOW US ON</p>
+                <h3>INSTAGRAM</h3>
+              </div>
             </div>
             <div className={styles.linkedin}>
-              <Image src="/assets/Instagram.svg" alt="Message icon" height={100} width={100}></Image>
-              <p>FIND US ON</p>
-              <h3>LINKEDIN</h3>
+              <Link href="https://www.linkedin.com/company/luminous-life-foundation/">
+              <Image src="/assets/Linkedin.svg" alt="Message icon" height={50} width={50}></Image>
+              </Link>
+              <div>
+                <p>FIND US ON</p>
+                <h3>LINKEDIN</h3>
+              </div>
             </div>
           </div>
         </div>
       </div>
       <div className={styles.bottomDiv}>
         <div className={styles.slide_info}>
-          <Image src="/assets/star.svg" alt="Star icon" width={23} height={23} />
+          <Image src="/assets/star.svg" alt="Star icon" width={20} height={20} />
           <p>Empowering Communities, Illuminating Futures</p>
-          <Image src="/assets/star.svg" alt="Star icon" width={23} height={23} />
+          <Image src="/assets/star.svg" alt="Star icon" width={20} height={20} />
           <p>Empowering Communities, Illuminating Futures</p>
-          <Image src="/assets/star.svg" alt="Star icon" width={23} height={23} />
+          <Image src="/assets/star.svg" alt="Star icon" width={20} height={20} />
           <p>Empowering Communities, Illuminating Futures</p>
-          <Image src="/assets/star.svg" alt="Star icon" width={23} height={23} />
+          <Image src="/assets/star.svg" alt="Star icon" width={20} height={20} />
           <p>Empowering Communities, Illuminating Futures</p>
-          <Image src="/assets/star.svg" alt="Star icon" width={23} height={23} />
+          <Image src="/assets/star.svg" alt="Star icon" width={20} height={20} />
           <p>Empowering Communities, Illuminating Futures</p>
         </div>
         <p className={styles.rights}>&copy; Luminous Life Foundation. All Rights Reserved.</p>
