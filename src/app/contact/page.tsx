4d667494b7951c18fea434e0b6dd1bf7069dc42f
Jose-henry <joseph.henry@stu.cu.edu.ng>
2024-06-28 20:19:48 +0100

    new

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,17 +1,13 @@
 import { Metadata } from "next";
-
-
-
+import Form from "@/components/contact_components/contact_form/conatct_form";
+import Picture from "@/components/contact_components/picture";
+import Final from "@/components/contact_components/contact_about/contact_about";
 
 export const metadata: Metadata = {
   title: "Contact",
 };
 
 
-
-import Form from "@/components/contact_components/contact_form/conatct_form";
-import Picture from "@/components/contact_components/picture";
-import Final from "@/components/contact_components/contact_about/contact_about";
 
 export default function Contact() {
   return (
