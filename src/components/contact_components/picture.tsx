d1d546a9e06cf4b536fe2ee5288d3bdc8deaa5ca
Oguibe Fortune Oluomachukwu <tech.eagle.labs@gmail.com>
2024-06-28 18:07:17 +0100

    messages

--- src/components/contact_components/picture.tsx
+++ src/components/contact_components/picture.tsx
@@ -1,6 +1,4 @@
-"use client";
 import React from 'react';
-import Typewriter from 'typewriter-effect';
 
 const Picture: React.FC = () => {
     return (
@@ -12,18 +10,8 @@ const Picture: React.FC = () => {
             width: '100%',
             height: '500px', 
             borderBottom: '2px solid white',
-            position: 'relative',
         }}>
         </div>
-        <p className='absolute bottom-[200px] left-5 text-[#622D25] opacity-[40%] text-[20px] font-bold'>
-        <Typewriter
-            options={{
-                strings: ['Lighting Up the Dark Places!!!'],
-                autoStart: true,
-                loop: true,
-            }}
-            />
-        </p>
         </div>
     );
 }
