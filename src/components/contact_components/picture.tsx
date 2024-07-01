97d58153b67b384d6ea43f0feee812d73e4324b9
Oguibe Fortune Oluomachukwu <tech.eagle.labs@gmail.com>
2024-07-01 11:05:14 +0100

    changes

--- src/components/contact_components/picture.tsx
+++ src/components/contact_components/picture.tsx
@@ -1,17 +1,17 @@
+import Image from 'next/image';
 import React from 'react';
 
 const Picture: React.FC = () => {
     return (
         <div>
             <div style={{ 
-            backgroundImage: 'url("/assets/office2.jpg")',
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
+            position: 'relative',
             width: '100%',
             height: '500px', 
             borderBottom: '2px solid white',
             marginTop: '58px',
         }}>
+            <Image src="/assets/office2.jpg" alt="Office" fill style={{objectFit: 'cover'}}></Image>
         </div>
         </div>
     );
